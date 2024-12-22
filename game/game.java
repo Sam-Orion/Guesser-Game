8fccea7a9abbe58f5da61a69780edbfac259e36d
Sam-Orion <121442461+Sam-Orion@users.noreply.github.com>
2024-12-22 16:57:53 +0530

    Added Guesser Class

---
+++ game/game.java
@@ -0,0 +1,18 @@
+package game;
+
+import java.util.Scanner;
+
+class Guesser {
+    int GuesserNum;
+    public int GuesserNumb() {
+        Scanner scan = new Scanner(System.in);
+        GuesserNum = scan.nextInt();
+        return GuesserNum;
+    }
+}
+
+public class game {
+    public static void main(String[] args) {
+
+    }
+}
